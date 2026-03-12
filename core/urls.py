3864b0dc9a1a77d1fab94a1173c87919d0a68336
Moshiur222 <md.moshiurrahiurrahman.et@gmail.com>
2026-03-12 14:25:30 +0600

    final commit

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,8 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
-from django.conf import settings
 from django.views.static import serve
+from django.conf.urls.static import static
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
